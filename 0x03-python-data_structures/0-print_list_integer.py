079f44bcf54847382b716a7426dee7f2905b5df7
marvins14 <nkoromacilinus@gmail.com>
2022-09-01 19:13:00 -0700

    added

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+
+    """PRINT ALL INTEGERS  OF A LIST."""
+    for i in my_list:
+    print("{ } \n {}".format(i))
